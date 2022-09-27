b112489a14c234d3c985d2ffccc92d9fe6b1ffdb
u.sadullaev <u.sadullaev@itransition.com>
2022-09-27 19:49:55 +0500

    Fix karma

--- karma.conf.js
+++ karma.conf.js
@@ -34,13 +34,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome", "ChromeHeadlessCI"],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: "ChromeHeadless",
-        flags: ["--no-sandbox"],
-      },
-    },
+    browsers: ["Chrome", "ChromeHeadless"],
     singleRun: false,
     restartOnFileChange: true,
   });
