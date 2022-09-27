c2eab31c7518850d01315bd8d6a0d69147a3f15d
u.sadullaev <u.sadullaev@itransition.com>
2022-09-27 19:34:15 +0500

    Fix karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,37 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+
+@josh-developer
+josh-developer
+/
+CI-CD-practice
+Public
+Code
+Issues
+Pull requests
+1
+Actions
+Projects
+Wiki
+Security
+1
+Insights
+Settings
+We found potential security vulnerabilities in your dependencies.
+Only the owner of this repository can see this message.
+
+CI-CD-practice/karma.conf.js /
+
+u.sadullaev run prettier
+Latest commit 966cd01 20 hours ago
+ History
+ 0 contributors
+41 lines (40 sloc)  1.38 KB
+
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
@@ -34,14 +68,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome_without_security"],
-    // you can define custom flags
-    customLaunchers: {
-      Chrome_without_security: {
-        base: "ChromeHeadless",
-        flags: ["--no-sandbox"],
-      },
-    },
+    browsers: ["Chrome", "ChromeHeadlessNoSandbox"],
     singleRun: false,
     restartOnFileChange: true,
   });
