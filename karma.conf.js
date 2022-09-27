5bc7cb03b58b653e731bf5ed1846ab6a876c8c05
u.sadullaev <u.sadullaev@itransition.com>
2022-09-27 16:27:04 +0500

    fix karma chrome

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ["Chrome_without_security"],
+    // you can define custom flags
+    customLaunchers: {
+      Chrome_without_security: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"],
+      },
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
